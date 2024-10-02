a68e5d02a63d5134534aa28c89434d4012208a67
LiHarviK <steve2002minecraft@gmail.com>
2024-10-02 22:33:20 +0300

    Add files via upload
    Практическое задание по уроку "Базовые структуры данных"

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello World")
+print(12 + 89 - 1)
+
+print(9 ** 0.5 * 5) # 1st program
+
+print(9.99 > 9.98 and 1000 != 1000.1) # 2nd program
+
+print(2 * 2 + 2) #3rd program
+print(2 * (2 + 2))
+print(2 *2 + 2 == 2 * (2+2))
+
+print('123.456') #4rd program
+print(float('123.456') * 10)
+
+print(int(123.456)) #5th program
